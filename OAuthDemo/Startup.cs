2c388798172ed766cbcf1da0cbca682a9644ead5
krunalyadav <krunal.yadav18@gmail.com>
2016-05-09 14:30:05 +0530

    test

---
+++ OAuthDemo/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OAuthDemo.Startup))]
+namespace OAuthDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var cookieOptions = new CookieAuthenticationOptions
+            {
+                LoginPath = new PathString("/Account/Login")
+            };
+            app.UseCookieAuthentication(cookieOptions);
+        }
+    }
+}
